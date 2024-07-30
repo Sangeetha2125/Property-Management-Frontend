921e012f490f7a77b1d0f05221f410c20495594e
S <shridharshini.s@sagentlending.com>
2024-07-30 11:25:24 +0530

    working on home page

--- src/pages/shared/LandingPage copy.tsx
+++ src/pages/shared/LandingPage copy.tsx
@@ -19,7 +19,7 @@ import image3 from "../../assets/image3.jpg";
 export const Hero = () => {
   return (
     <div className="flex min-h-screen w-full flex-col poppins-light">
-      <div className="flex flex-col sm:gap-4 sm:py-4 ">
+      <div className="flex flex-col sm:gap-4 sm:pt-4">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <img width={90} height={50} src={logo} alt="logo" />
           <div className="ml-auto flex items-center gap-4 w-1/5">
@@ -39,10 +39,10 @@ export const Hero = () => {
         <Separator />
       </div>
 
-      <div className="p-4 ">
+      <div className="">
         <div className="h-screen rounded-3xl w-11/12 flex items-center justify-center Home">
           {/* <p className="text-4xl text-center m-4 font-semibold text-white">Trusted Place To Find A Home For Your Family</p> */}
-          <div className="w-1/2 p-4 bg-white rounded-lg bg-opacity-70">
+          <div className="w-1/2 p-4 bg-white rounded-2xl bg-opacity-70">
             <p className="text-4xl text-center m-4 font-semibold leading-10">
             Trusted Place To Find A Home For Your Family  
             </p>
